09da375677642ff4d630430b963305f7767771e8
Claudia De Fazio <ccjd544@hotmail.com>
2021-03-18 22:05:13 -0400

    Added working menu bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  showMenu = false;
   title = 'BachelorPool';
+
+  toggleMenu(){
+    this.showMenu = !this.showMenu;
+  }
+
 }
+
